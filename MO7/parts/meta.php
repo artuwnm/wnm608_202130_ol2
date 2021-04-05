85eeda9d79699253221a83ea35c02e8fbc9b4098
yuanyuan-pen <78908915+yuanyuan-pen@users.noreply.github.com>
2021-04-06 00:06:37 +0800

    Add files via upload

---
+++ MO7/parts/meta.php
@@ -0,0 +1,10 @@
+
+
+<meta name="viewport" content="width=device-width">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./lib/css/styleguide.css">
+
+
+<link rel="stylesheet"href="./css/storetheme.css">
