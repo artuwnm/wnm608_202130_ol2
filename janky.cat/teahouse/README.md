b48687755ec0bfc063949ddb69c98e3e83429759
hear-colors <78772436+hear-colors@users.noreply.github.com>
2021-04-07 19:32:02 -0500

    Module 7 Updates

---
+++ janky.cat/teahouse/README.md
@@ -0,0 +1,3 @@
+# Cat Janky
+
+- http://catjankydesign.com
---
+++ janky.cat/teahouse/parts/navbar.php
@@ -0,0 +1,28 @@
+
+<header class="navbar">
+		<div class="display-flex">
+
+			<nav class="flex-stretch nav">
+				<ul class="container-row display-flex">
+					<li> <a href="#">Shop Teas</a></li>
+				</ul>
+			</nav>
+
+				<div class="flex-stretch">
+					<h1 class="logo">TEA HOUSE</h1>
+				</div>
+
+			<nav class="flex-none nav">
+				<ul class="container-row display-flex">
+					<li> <a href="#">Learn</a></li>
+					<li> <a href="#">About</a></li>
+				</ul>
+			</nav>
+			
+			<div class="flex-none"></div>
+			
+			<div class="cart"> 
+				<a href="#"> <img src="img/cart.png"></a>
+			</div>
+		</div>
+</header>
