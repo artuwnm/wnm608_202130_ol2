590e99873b3a8bb44eaef0c27db214369e8f8f14
ChenxiYY <79078476+ChenxiYY@users.noreply.github.com>
2021-04-11 20:30:13 -0700

    Add files via upload

---
+++ Wang.Yoyo/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title>Film Store</title>
+	<?php include "parts/meta.php"; ?>
+</head>
+
+<body>
+	
+	<?php include "parts/navbar.php"; ?>
+
+
+	
+		<h3>Nikon Camera</h3>
+
+    	<div class="grid gap">
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure">
+    		        <img src="img/Nikon.jpeg" alt="media-image">
+    		        <figcaption>NIKON Z 50 DX-FORMAT</figcaption>
+    	        </figure>
+    		</div>
+
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure">
+    		        <img src="img/Nikon2.jpeg" alt="media-image">
+    		        <figcaption>NIKON Z 7II FX-FORMAT</figcaption>
+    	        </figure>
+    		</div>
+
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure">
+    		        <img src="img/Nikon3.png" alt="media-image">
+    		        <figcaption>NIKON D7500 DX FORMAT</figcaption>
+    	        </figure>
+    		</div>
+    	</div>
+    	<div class="view-window" style="background-image: url('img/background.jpg')">
+		
+	</div>
+
+
+    	<h3>Canon Camera</h3>
+    	<div class="grid gap">
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure product">
+    		<img src="img/Canon1.jpeg" alt="media-image">
+    		<figcaption>
+    			<div>CANON EOS R5 FULL-FRAME</div>
+    			<div>$3899.99</div>
+    		</figcaption>
+    	</figure>
+    		</div>
+
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure product">
+    		<img src="img/Canon2.png" alt="media-image">
+    		<figcaption>
+    			<div>CANON 5D MARK IV</div>
+    			<div>$2499.99</div>
+    		</figcaption>
+    	</figure>
+    		</div>
+
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure product">
+    		<img src="img/Canon3.jpeg" alt="media-image">
+    		<figcaption>
+    			<div>CANON EOS R6 FULL-FRAME</div>
+    			<div>$2499.99</div>
+    		</figcaption>
+    	</figure>
+    		</div>
+    	</div>
+
+    	<h3>Sony Camera</h3>
+    	<div class="grid gap">
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure product-overlay">
+    		<img src="img/sony1.jpeg" alt="media-image">
+    		<figcaption>
+    			<div class="caption-body">
+    			<div>SONY ALPHA A1 FULL FRAME</div>
+    			<div>$6499.99</div>
+    		</div>
+    		</figcaption>
+    	</figure>
+    		</div>
+
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure product-overlay">
+    		<img src="img/sony2.jpeg" alt="media-image">
+    		<figcaption>
+    			<div class="caption-body">
+    			<div>SONY CYBER-SHOT DIGITAL CAMERA RX100 VII</div>
+    			<div>$1299.99</div>
+    		</div>
+    		</figcaption>
+    	</figure>
+    		</div>
+
+    		<div class="col-xs-12 col-md-4">
+    			<figure class="figure product-overlay">
+    		<img src="img/sony3.png" alt="media-image">
+    		<figcaption>
+    			<div class="caption-body">
+    			<div>SONY ALPHA A7III FULL FRAME</div>
+    			<div>$2199.99</div>
+    		</div>
+    		</figcaption>
+    	</figure>
+    		</div>
+    	</div>
+    </div>    	
+  </div>
+
+
+</body>
+</html>
