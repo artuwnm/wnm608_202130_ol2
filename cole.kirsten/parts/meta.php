54e04ce7a9f56d5fe4573485b56e738ad463e1dc
Kirsten Cole <kirsten.cole@gmail.com>
2021-04-12 00:43:08 -0400

    eighth commit

--- cole.kirsten/parts/meta.php
+++ cole.kirsten/parts/meta.php
@@ -1,5 +1,7 @@
 	
 <meta name="viewport" content="width=device-width">
 
+<base href="http://kirstenmaher.com/aau/wnm608/cole.kirsten/">
+
 <link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
 <link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
