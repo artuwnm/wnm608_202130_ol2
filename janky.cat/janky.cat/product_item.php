d5a7e80619a43cbba2f06dee1cecb2d830a24f94
hear-colors <78772436+hear-colors@users.noreply.github.com>
2021-04-20 11:57:11 -0500

    MODULE 10

--- janky.cat/janky.cat/product_item.php
+++ janky.cat/janky.cat/product_item.php
@@ -39,14 +39,59 @@ $image_elements = array_reduce($images,function($r,$o){
 				</div>
 			</div>
 			<div class="col-xs-12 col-md-5">
-				<div class="card soft">
-					<h2 class="product-name"><?= $product->name ?></h2>
-					<div class="product-price">&dollar;<?= $product->price ?></div>
+				<div class="card flat">
+					<div class="card-section">
+						<h2 class="product-name"><?= $product->name ?></h2>
+						<div class="product-price">&dollar;<?= $product->price ?></div>
+					</div>
+
+					<div class="card-section">
+						<label for="product-amount" class="form-label">Amount</label>
+							<div class="form-select" id="product-amount">
+								<select>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+									<option>6</option>
+									<option>7</option>
+									<option>8</option>
+									<option>9</option>
+									<option>10</option>
+									<option>11</option>
+									<option>12</option>
+								</select>
+							</div>
+					</div>
+
+					<div class="card-section">
+						<div>
+						<a href="product_added_to_cart.php?id=<?= $product->id ?>" class="form-button">Add to Cart</a>	
+						</div>
+					</div>
 				</div>
 			</div>
+		</div>
+
+		<div class="card soft dark">
+			<p><?= $product->description?></p>
+
 		</div>
 	</div>
 
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
