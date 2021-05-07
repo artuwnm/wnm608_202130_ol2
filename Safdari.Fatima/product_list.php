238fabf893d94432f215f1dffe1e14998106a6df
fatemarc <fatemeh@fatemarcdesign.com>
2021-05-07 11:08:49 -0700

    module 12

--- Safdari.Fatima/product_list.php
+++ Safdari.Fatima/product_list.php
@@ -15,6 +15,10 @@ include_once "parts/templates.php";
 	<title>Product List</title>
 
 	<?php include "parts/meta.php"; ?>
+     <script src="lib/js/functions.js"></script>
+     <script src="js/templates.js"></script>
+     <script src="js/product_list.js"></script>
+
 	
 
 </head>
@@ -25,245 +29,55 @@ include_once "parts/templates.php";
      
 <div class="container">
      
-          <div class="form-control">
-               <form>
-                     <input type="search" class="hotdog" placeholder="Search">
-               </form>
+         
           </div>
-</div>
-<div class="container">
+          <div class="container">
 		
 			<h2>Product List</h2>
-		
-          <?php
-      
+               <div class="form-control">
+                    <form class="hotdog light" id="product-search">
+                         <input type="search" placeholder="Search Products">
+                         
+                    </form>
+               </div>
 
-
-      $result = makeQuery(
-          makeConn(),
-          "
-          SELECT *
-          FROM `products`
-          ORDER BY `price` DESC
-          LIMIT 12
-          "
-     );
-     echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div";
-          ?>
-	</div>
-</div>
-     		<!--    <div class="grid gap">
-     		<div class="col-xs-12 col-md-4">
-     				<figure class="figure">
-     			<img src='img/bamiyeh.jpg' alt="">
-     			<figcaption><li><a href="product_item.php?id=1">Bamiyeh</a></li>
-                         <div>$12.99</div>
-     			</figcaption>
-     		</figure>
-     			</div>
-     			<div class="col-xs-12 col-md-4">
-     				<figure class="figure">
-     			<img src='img/honey_brittle.jpg' alt="">
-     			<figcaption><li><a href="product_item.php?id=2">Honey Brittle</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-     		</figure>
-     			</div>
-     			<div class="col-xs-12 col-md-4">
-     				<figure class="figure">
-     			<img src='img/kolompeh.jpg' alt="">
-     			<figcaption><li><a href="product_item.php?id=3">Kolompeh</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-     		</figure>
-     		</div>
-     	</div>
-               <div class="container" id="tableofcontents">
-               <h2>Cookies</h2>
-                    <div class="container">
-        <div class="card soft">
-               <h2>Product List</h2>
-
-               <ul>
-                    <li><a href="product_item.php?id=7">Product 7</a></li>
-                    <li><a href="product_item.php?id=8">Product 8</a></li>
-                    <li><a href="product_item.php?id=9">Product 9</a></li>
-                    
-               </ul>
-          </div>
-     </div>
-               <div class="grid gap">
-                    <div class="col-xs-12 col-md-4">
-                         <figure class="figure">
-                    <img src='img/qorabieh.jpg' alt="">
-                    <figcaption><li><a href="product_item.php?id=4">Qorabieh</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-               </figure>
+               <div class="form-control">
+                    <div class="card soft">
+                    <div class="display-flex">
+                     <div class="flex-stretch display-flex">
+                    <div class="flex-none">
+                         <button data-filter="category" data-value="" type="button" class="form-button">All</button>
                     </div>
-                    <div class="col-xs-12 col-md-4">
-                         <figure class="figure">
-                    <img src='img/raisin cookies.jpg' alt="">
-                    <figcaption><li><a href="product_item.php?id=5">Raisin Cookies</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-               </figure>
+                    <div class="flex-none">
+                         <button data-filter="category" data-value="gift" type="button" class="form-button">Gift</button>
                     </div>
-                    <div class="col-xs-12 col-md-4">
-                         <figure class="figure">
-                    <img src='img/feather cookies.jpg' alt="">
-                    <figcaption><li><a href="product_item.php?id=6">Feather cookis</a></li>
-                         <div>$12.99</div>
-
-                    </figcaption>
-               </figure>
+                    <div class="flex-none">
+                         <button data-filter="category" data-value="cookies" type="button" class="form-button">Cookies</button>
+                    </div>
+                     <div class="flex-none">
+                         <button data-filter="category" data-value="special" type="button" class="form-button">Special</button>
+                    </div>
+                  </div>  
+        
+                 <div class="flex-none">
+                    <div class="form-select">
+                              <select class="js-sort">
+                                   <option value="1">Newest</option>
+                                   <option value="2">Oldest</option>
+                                   <option value="3">Least Expensive</option>
+                                   <option value="4">Mose Expensive</option>
+                                  
+                              </select>
+                         </div>
+                    </div>
                </div>
           </div>
-
-
-		<div class="container" id="tableofcontents">
-     		<h2>Special</h2>
-     			<div class="container">
-		<div class="card soft">
-			<h2>Product List</h2>
-
-			<ul>
-				<li><a href="product_item.php?id=4">Product 4</a></li>
-				<li><a href="product_item.php?id=5">Product 5</a></li>
-				<li><a href="product_item.php?id=6">Product 6</a></li>
-				
-			</ul>
-		</div>
-	</div>
-     		<div class="grid gap">
-     			<div class="col-xs-12 col-md-4">
-     				<figure class="figure">
-     			<img src='img/marzipan.jpg' alt="">
-     			<figcaption><li><a href="product_item.php?id=7">Marzipan</a></li>
-                         <div>$12.99</div>
-     			</figcaption>
-     		</figure>
-     			</div>
-     			<div class="col-xs-12 col-md-4">
-     				<figure class="figure">
-     			<img src='img/qottab.jpg' alt="">
-     			<figcaption><li><a href="product_item.php?id=8">Qottab</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-     		</figure>
-     			</div>
-     			<div class="col-xs-12 col-md-4">
-     				<figure class="figure">
-     			<img src='img/chickpea_gift.jpg' alt="">
-     			<figcaption><li><a href="product_item.php?id=9">Chickpea</a></li>
-                         <div>$12.99</div>
-
-                    </figcaption>
-     		</figure>
-     		</div>
-     	</div>
+     </div>
+		<div class='productlist grid gap'></div>
+       
 	
-          <div class="container" id="tableofcontents">
-               <h2>Gift</h2>
-                    <div class="container">-->
-          <!--<div class="card soft">
-               <h2>Product List</h2>
-
-               <ul>
-                    <li><a href="product_item.php?id=7">Product 7</a></li>
-                    <li><a href="product_item.php?id=8">Product 8</a></li>
-                    <li><a href="product_item.php?id=9">Product 9</a></li>
-                    
-               </ul>
-          </div>
-     </div>
-
-               <div class="grid gap">
-                    <div class="col-xs-12 col-md-4">
-                         <figure class="figure">
-                    <img src='img/kerman_gift_1.jpg' alt="">
-                    <figcaption><li><a href="product_item.php?id=10">Kerman Gift</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-               </figure>
-                    </div>
-                    <div class="col-xs-12 col-md-4">
-                         <figure class="figure">
-                    <img src='img/6.jpg' alt="">
-                    <figcaption><li><a href="product_item.php?id=11">Summer Days Gift</a></li>
-                         <div>$12.99</div>
-
-                    </figcaption>
-               </figure>
-                    </div>
-                    <div class="col-xs-12 col-md-4">
-                         <figure class="figure">
-                    <img src='img/kolompeh_gift.jpg' alt="">
-                    <figcaption><li><a href="product_item.php?id=12">Eshgh Gift</a></li>
-                         <div>$12.99</div>
-                    </figcaption>
-               </figure>
-               </div>
-          </div>
-     </div>
 </div>
-
-
-  <h4><span>We Offer</span></h4>    
-<div class="container" id="tableofcontents">
-              
-      <div class="card green"> 
-      
-         <div class="grid gap">
-            <div class="col-xs-6 col-md-3">   
-               <figure class="figure product-overlay">
-                    <img src='img/7.jpg' alt="">
-                <figcaption>
-                    <div class="caption-body">
-                         <div>Product Name</div>
-                          <div>$14.99</div>
-                    </div>
-               </figcaption>
-
-               </figure>
-                    </div>
-                    <div class="col-xs-6 col-md-3">
-                         <figure class="figure product-overlay">
-                         <img src='img/16.jpg' alt="">
-                         <figcaption>
-                         <div class="caption-body">
-                              <div>Product Name</div>
-                              <div>$13.99</div>
-               </figcaption>
-               </figure>
-                    </div>
-
-                    <div class="col-xs-6 col-md-3">
-                         <figure class="figure product-overlay">
-                    <img src='img/berenj.jpg' alt="">
-                    <figcaption>
-                         <div class="caption-body">
-                              <div>Product Name</div>
-                              <div>$11.99</div>
-                         </div>
-                    </figcaption>
-
-               </figure> 
-                    
-               </div>
-           <div class="col-xs-6 col-md-3">
-                         <figure class="figure product-overlay">
-                    <img src='img/82.jpg' alt="">
-                    <figcaption>
-                         <div class="caption-body">
-                              <div>Product Name</div>
-                              <div>$9.99</div>
-                         </div>
-                    </figcaption>
-               </figure>              
-          </div>
-
-</div>-->
+     
 
 
 
