df0321d657d7f9ffe898a4345264edbe7119d01a
yuanyuan-pen <78908915+yuanyuan-pen@users.noreply.github.com>
2021-05-07 23:37:07 +0800

    Add files via upload

---
+++ MO12/index.php
@@ -0,0 +1,196 @@
+<?php include_once "lib/php/functions.php"; ?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>24H CLOSED</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
+
+	
+
+	
+	<link rel="stylesheet" href="css/storetheme.css">
+	<link rel="stylesheet" href="./lib/css/styleguide.css">
+	<link rel="stylesheet" href="./lib/css/gridsystem.css">
+
+	
+
+
+</head>
+<body>
+	
+
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>24H CLOSED</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="flex-none nav">
+				<ul class="container display-flex">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
+					<li><a href="#figure3">About</a></li>
+					<li><a href="product_cart.php">Cart</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<div class="view-window" style="background-image:url('https://i.postimg.cc/kX0md7CL/2021-03-06-3-00-53.png')">
+		
+
+	</div>
+
+
+	
+
+	<div class="container">								
+		<div class="card hard">
+			<h2 id="figure1">Best Recommend</h2>
+			<div class="grid gap">
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/MpQWfH56/IMG-8208.jpg">	
+						<figcaption>$15.00</figcaption>		
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/NFFtsjLw/IMG-8209.jpg">
+						<figcaption>$29.97</figcaption>			
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/T1s6pvW5/IMG-8210.jpg">
+						<figcaption>$29.97</figcaption>
+					</figure>
+				</div>	
+			</div>									
+		</div>
+			
+	</div>
+
+	
+		
+	
+
+
+
+
+
+
+	<div class="view-window two" style="background-image:url('https://i.postimg.cc/KYBcWQWS/2021-03-06-4-50-32.png')">
+		
+
+	</div>
+
+
+	
+
+	<div class="container">								
+		<div class="card hard">
+			<h2 id="figure2">Phone Case</h2>
+			<div class="grid gap">
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/pTT06hpz/IMG-8220.jpg">	
+						<figcaption>$29.97</figcaption>		
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/FKs5hKYs/IMG-8223.jpg">
+						<figcaption>$29.97</figcaption>			
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/rFSvW4Ww/IMG-8224.jpg">
+						<figcaption>$29.97</figcaption>
+					</figure>
+				</div>	
+			</div>									
+		</div>
+			
+	</div>	
+		
+
+		
+	
+	<div class="container">								
+		<div class="card hard">
+			<h2 id="figure3">Airpods case</h2>
+			<div class="grid gap">
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/Bb2wTWjV/IMG-8225.jpg">	
+						<figcaption>$33.00</figcaption>		
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/cLx5nb0F/IMG-8226.jpg">
+						<figcaption>$33.00</figcaption>			
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/gJVv4GK8/IMG-8231.jpg">
+						<figcaption>$30.00</figcaption>
+					</figure>
+				</div>	
+			</div>									
+		</div>
+			
+	</div>	
+
+
+
+
+	
+
+
+	<div class="container">								
+		<div class="card hard">
+			<h2 id="figure4">Badge</h2>
+			<div class="grid gap">
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/Kj93sj3k/IMG-8228.jpg">	
+						<figcaption>$18.78</figcaption>		
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/PqkJyYVV/IMG-8227.jpg">
+						<figcaption>$6.60</figcaption>			
+					</figure>
+				</div>
+			
+				<div class="col-xs-12 col-md-4">
+					<figure class="figure">
+						<img src="https://i.postimg.cc/j2CS2XC2/IMG-8229.jpg">
+						<figcaption>$6.13</figcaption>
+					</figure>
+				</div>	
+			</div>									
+		</div>
+			
+	</div>
+
+
+		
+
+		
+		
+
+</body>
+</html>
