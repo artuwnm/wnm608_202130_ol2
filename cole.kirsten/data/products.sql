e8cd455df70f194a3a94846684cc0e032c8dbba0
Kirsten Cole <kirsten.cole@gmail.com>
2021-05-09 23:53:59 -0700

    Twelfth commit

--- cole.kirsten/data/products.sql
+++ cole.kirsten/data/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 24, 2021 at 02:36 PM
+-- Generation Time: May 09, 2021 at 10:37 PM
 -- Server version: 5.6.49-cll-lve
 -- PHP Version: 7.3.6
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `KCOLEWNM608`
+-- Database: `kcolewnm608`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +34,7 @@ CREATE TABLE `products` (
   `description` varchar(128) NOT NULL,
   `details` text NOT NULL,
   `price` decimal(10,2) NOT NULL,
+  `category` varchar(32) NOT NULL,
   `date_create` datetime NOT NULL,
   `date_modify` datetime NOT NULL,
   `thumbnail` varchar(128) NOT NULL,
@@ -44,19 +45,19 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `description`, `details`, `price`, `date_create`, `date_modify`, `thumbnail`, `images`) VALUES
-(1, 'Salmon', 'Fresh \"King\" Salmon', 'Wild caught Alaskan salmon.  Price per pound.', 19.99, '2021-04-17 23:00:50', '2021-04-17 23:00:50', 'img/salmon1.jpg', 'img/salmon1.jpg,images/salmon2.jpg'),
-(2, 'Tuna', 'Fresh Yellowfin Tuna', 'Ahi Yellowfin Tuna sustainably caught from Hawaii.  Price per pound.', 29.99, '2021-04-17 23:01:10', '2021-04-17 23:01:10', 'img/tuna1.jpg', 'img/tuna1.jpg'),
-(3, 'Shrimp', 'Fresh Jumbo Black Tiger Prawns', 'Wild Jumbo Black Tiger Prawns sustainably caught from Mexico.  Price per pound.', 26.99, '2021-04-17 23:01:40', '2021-04-17 23:01:40', 'img/shrimp1.jpg', 'img/shrimp1.jpg'),
-(4, 'Octopus', 'Wild frozen octopus', 'Wild frozen octopus sustainably caught from Portugal.  Price per pound.', 37.99, '2021-04-17 23:01:52', '2021-04-17 23:01:52', 'img/octopus1.jpg', 'img/octopus1.jpg'),
-(5, 'Scallops', 'Wild Sea Scallops', 'Wild Sea Scallops caught on the Atlantic coastline.  Price per pound.', 36.99, '2021-04-17 23:02:03', '2021-04-17 23:02:03', 'img/scallops1.jpg', 'img/scallops1.jpg'),
-(6, 'Squid', 'Wild Squid Tubes and Tentacles', 'Wild frozen squid with tubes and tentacles sustainably caught.  Price per pound.', 14.99, '2021-04-17 23:02:17', '2021-04-17 23:02:17', 'img/squid1.jpg', 'img/squid1.jpg'),
-(7, 'Snapper', 'American Red Snapper', 'American Red Snapper sustainably caught off the Atlantic coastline.  Price per pound.', 36.99, '2021-04-17 23:02:30', '2021-04-17 23:02:30', 'img/snapper1.jpg', 'img/snapper1.jpg'),
-(8, 'Mussels', 'Whitewater Blue Wild Mussels', 'Whitewater Blue Wild Mussels sustainably caught off the New England coastline.  Price per pound.', 7.99, '2021-04-17 23:02:41', '2021-04-17 23:02:41', 'img/mussels1.jpg', 'img/mussels1.jpg'),
-(9, 'Oysters', 'Naked Cowboy Wild Oysters', 'Naked Cowboy Wild Oysters sustainably caught off the New York Long Island Sound.  Price per pound.', 26.99, '2021-04-17 23:02:56', '2021-04-17 23:02:56', 'img/oyster1.jpg', 'img/oyster1.jpg,img/oyster2.jpg'),
-(10, 'Crab', 'Red Alaskan King Crab Frozen', 'Red Alaskan King Crab sustainably caught off the coast of Alaska.  Price per pound.', 64.99, '2021-04-17 23:03:10', '2021-04-17 23:03:10', 'img/crab1.jpg', 'img/crab1.jpg'),
-(11, 'Sardines', 'Wild Frozen Sardines', 'Wild frozen sardines sustainably caught off the coast of Portugal.  Price per pound.', 12.99, '2021-04-17 23:03:20', '2021-04-17 23:03:20', 'img/sardines1.jpg', 'img/sardines1.jpg'),
-(12, 'Lobster', 'North American Wild Lobster', 'Wild cold water lobster sustainably caught off the Atlantic coastline.  Price per pound.', 33.99, '2021-04-17 23:03:30', '2021-04-17 23:03:30', 'img/lobster1.jpg', 'img/lobster1.jpg');
+INSERT INTO `products` (`id`, `name`, `description`, `details`, `price`, `category`, `date_create`, `date_modify`, `thumbnail`, `images`) VALUES
+(1, 'Salmon', 'Fresh \"King\" Salmon', 'Wild caught Alaskan salmon.  Price per pound.', 19.99, 'Fish', '2021-04-17 23:00:50', '2021-04-17 23:00:50', 'img/salmon1.jpg', 'img/salmon1.jpg,images/salmon2.jpg'),
+(2, 'Tuna', 'Fresh Yellowfin Tuna', 'Ahi Yellowfin Tuna sustainably caught from Hawaii.  Price per pound.', 29.99, 'Fish', '2021-04-17 23:01:10', '2021-04-17 23:01:10', 'img/tuna1.jpg', 'img/tuna1.jpg'),
+(3, 'Shrimp', 'Fresh Jumbo Black Tiger Prawns', 'Wild Jumbo Black Tiger Prawns sustainably caught from Mexico.  Price per pound.', 26.99, 'Shellfish', '2021-04-17 23:01:40', '2021-04-17 23:01:40', 'img/shrimp1.jpg', 'img/shrimp1.jpg'),
+(4, 'Octopus', 'Wild frozen octopus', 'Wild frozen octopus sustainably caught from Portugal.  Price per pound.', 37.99, 'Fish', '2021-04-17 23:01:52', '2021-04-17 23:01:52', 'img/octopus1.jpg', 'img/octopus1.jpg'),
+(5, 'Scallops', 'Wild Sea Scallops', 'Wild Sea Scallops caught on the Atlantic coastline.  Price per pound.', 36.99, 'Shellfish', '2021-04-17 23:02:03', '2021-04-17 23:02:03', 'img/scallops1.jpg', 'img/scallops1.jpg'),
+(6, 'Squid', 'Wild Squid Tubes and Tentacles', 'Wild frozen squid with tubes and tentacles sustainably caught.  Price per pound.', 14.99, 'Fish', '2021-04-17 23:02:17', '2021-04-17 23:02:17', 'img/squid1.jpg', 'img/squid1.jpg'),
+(7, 'Snapper', 'American Red Snapper', 'American Red Snapper sustainably caught off the Atlantic coastline.  Price per pound.', 36.99, 'Fish', '2021-04-17 23:02:30', '2021-04-17 23:02:30', 'img/snapper1.jpg', 'img/snapper1.jpg'),
+(8, 'Mussels', 'Whitewater Blue Wild Mussels', 'Whitewater Blue Wild Mussels sustainably caught off the New England coastline.  Price per pound.', 7.99, 'Shellfish', '2021-04-17 23:02:41', '2021-04-17 23:02:41', 'img/mussels1.jpg', 'img/mussels1.jpg'),
+(9, 'Oysters', 'Naked Cowboy Wild Oysters', 'Naked Cowboy Wild Oysters sustainably caught off the New York Long Island Sound.  Price per pound.', 26.99, 'Shellfish', '2021-04-17 23:02:56', '2021-04-17 23:02:56', 'img/oyster1.jpg', 'img/oyster1.jpg,img/oyster2.jpg'),
+(10, 'Crab', 'Red Alaskan King Crab Frozen', 'Red Alaskan King Crab sustainably caught off the coast of Alaska.  Price per pound.', 64.99, 'Shellfish', '2021-04-17 23:03:10', '2021-04-17 23:03:10', 'img/crab1.jpg', 'img/crab1.jpg'),
+(11, 'Sardines', 'Wild Frozen Sardines', 'Wild frozen sardines sustainably caught off the coast of Portugal.  Price per pound.', 12.99, 'Fish', '2021-04-17 23:03:20', '2021-04-17 23:03:20', 'img/sardines1.jpg', 'img/sardines1.jpg'),
+(12, 'Lobster', 'North American Wild Lobster', 'Wild cold water lobster sustainably caught off the Atlantic coastline.  Price per pound.', 33.99, 'Shellfish', '2021-04-17 23:03:30', '2021-04-17 23:03:30', 'img/lobster1.jpg', 'img/lobster1.jpg');
 
 --
 -- Indexes for dumped tables
--- cole.kirsten/product_list.php
+++ cole.kirsten/product_list.php
@@ -10,6 +10,11 @@
 	<title>Product List</title>
 
 	<?php include "parts/meta.php"; ?>
+
+	 <script src="lib/js/functions.js"></script>
+     <script src="js/templates.js"></script>
+     <script src="js/product_list.js"></script>
+
 </head>
 <body>
 	
@@ -21,26 +26,41 @@
 
 	<div class="container">
 			<h2>Product List</h2>
+			<div class="form-control">
+                    <form class="hotdog dark" id="product-search">
+                         <input type="search" placeholder="Search Products">
+                    </form>
+            </div>
 
-
-			<?php
-
-
-
-			$result = makeQuery(
-				makeConn(),
-				"
-				SELECT *
-				FROM `products`
-				ORDER BY `date_create` DESC
-				LIMIT 12
-				"
-			);
-
-
-			echo "<div class='productlist grid gap'>",array_reduce($result,'productListTemplate'),"</div>";
-
-			?>
+            <div class="form-control">
+                <div class="card soft">
+                    <div class="display-flex">
+                    	<div class="flex-stretch display-flex">
+                    		<div class="flex-none">
+                        		<button data-filter="category" data-value="" type="button" class="form-button">All</button>
+                    		</div>
+                    		<div class="flex-none">
+                         		<button data-filter="Fish" data-value="fish" type="button" class="form-button">Fish</button>
+                    		</div>
+                    		<div class="flex-none">
+                         		<button data-filter="Shellfish" data-value="shellfish" type="button" class="form-button">Shellfish</button>
+                    		</div>
+                		</div>  
+        
+                		<div class="flex-none">
+                    		<div class="form-select">
+                              <select class="js-sort">
+                                   <option value="1">Newest</option>
+                                   <option value="2">Oldest</option>
+                                   <option value="3">Least Expensive</option>
+                                   <option value="4">Mose Expensive</option>
+                              </select>
+                    		</div>
+                 		</div>
+              		</div>
+            	</div>
+     		</div>
+			<div class='productlist grid gap'></div>
 	</div>
 
 
