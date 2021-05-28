dcc0cfe38d93979feceed70351dbc6bf3d387438
yuanyuan-pen <78908915+yuanyuan-pen@users.noreply.github.com>
2021-05-28 21:23:10 +0800

    Add files via upload

---
+++ MO15/parts/meta.php
@@ -0,0 +1,17 @@
+<meta name="viewport" content="width=device-width">
+<base href="http://pyuanyuan.com/MO14/">
+
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+
+
+
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
+
+	
+
+	
+<link rel="stylesheet" href="css/storetheme.css">
+<link rel="stylesheet" href="./lib/css/styleguide.css">
+<link rel="stylesheet" href="./lib/css/gridsystem.css">
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
